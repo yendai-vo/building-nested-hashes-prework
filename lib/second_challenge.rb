54db0ce17d991a4c0e880f750026364b331eee12
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 07:37:36 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,13 +5,12 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      },
+      }, 
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
       }}
-
 
 end
